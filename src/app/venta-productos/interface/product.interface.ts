91eac8166d3bc25f4f03ca25b9c86adf4c6e90fb
WilberthGamboa <wgcampos15@gmail.com>
2023-09-14 23:09:31 -0600

    reset data

---
+++ src/app/venta-productos/interface/product.interface.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    barcode : string
+    nombre: string
+    precio: number,
+    categoria:string
+}
--- src/app/venta-productos/venta-productos.component.ts
+++ src/app/venta-productos/venta-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './interface/product.interface';
 
 @Component({
   selector: 'app-venta-productos',
@@ -6,28 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./venta-productos.component.css']
 })
 export class VentaProductosComponent {
-Number(arg0: string): number {
-throw new Error('Method not implemented.');
-}
-  public data:any [] = [{
-    id:1,
-    nombre:'Tkt',
-    precio:'1000'
-  }]
+
+  public data:Product[] = [{
+    barcode:'1',
+    nombre:'TKT L MISIL',
+    precio:44,
+    categoria:'cerveza'
+  },
+  {
+    barcode:'2',
+    nombre:'XX LAGUER MISIL',
+    precio:45,
+    categoria:'cerveza'
+  },
+
+  {
+    barcode:'3',
+    nombre:'SUPERIOR MISIL',
+    precio:40,
+    categoria:'cerveza'
+  }
+
+]
 
   public venta:any [] = []
 
   public enviarMensaje(dinero:string){
     (window as any).electronAPI.setTitle('ola');
     const x = Number(dinero)
-    console.log(x)
+    
     this.data.forEach(element => {
-      if (element.id === x) {
+      if (element.barcode === dinero) {
         this.venta.push(element)
       }
       
     });
     console.log(this.venta);
+  }
+
+  public reiniciarVenta(){
+    this.venta = [];
   }
 
 }
