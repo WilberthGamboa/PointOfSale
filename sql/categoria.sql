df1aec5a52c6c19ee2e0521f3874cfde6f24a85a
WilberthGamboa <wgcampos15@gmail.com>
2023-10-07 15:51:43 -0600

    se genera el resultado total

--- sql/categoria.sql
+++ sql/categoria.sql
@@ -2,3 +2,4 @@ INSERT INTO categoria (categoriaName,createdAt,updatedAt)
 VALUES  
  
  ('Cerveza','2023-10-05 10:00:00', '2023-10-05 10:00:00')
+  ('Abarrotes','2023-10-05 10:00:00', '2023-10-05 10:00:00')
